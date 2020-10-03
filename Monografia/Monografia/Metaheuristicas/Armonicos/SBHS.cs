536a65ab7b1fab25e04f1c27673155f8cc5722c8
Guzman95 <santiagog@unicauca.edu.co>
2020-10-02 21:16:29 -0500

    Parte actualizada SBHS

--- Monografia/Monografia/Metaheuristicas/Armonicos/SBHS.cs
+++ Monografia/Monografia/Metaheuristicas/Armonicos/SBHS.cs
@@ -7,57 +7,149 @@ namespace Monografia.Metaheuristicas.Armonicos
 {
     class SBHS : Algorithm
     {
-        public static int[][] InicializarMemoria()
+
+
+        private int[] reparar(p_mediana x, int[] X)
         {
-            var aleatorio = new Random(255);
-            int[][] HarmonyMemory = new int[10][]; //HMS
-            for (int i = 0; i < 10; i++)
-            {
-                HarmonyMemory[i] = new int[10]; //Tamaño de la población
-                for (int j = 0; j < 10; j++)
-                {
-                    if (aleatorio.NextDouble() < 0.5)
-                    {
-                        HarmonyMemory[i][j] = 1;
-
-                    }
-                    else
-                        HarmonyMemory[i][j] = 0;
-                }
-                //Console.WriteLine("Antes de reparar "+ HarmonyMemory[i][0]);
-                reparar(HarmonyMemory[i]);
-                //Console.WriteLine("Despues de reparar de reparar"+ HarmonyMemory[i][0]);
-            }
-            return HarmonyMemory;
+            int[] Xnew = new int[X.Length];
+            Xnew = X;
+            return Xnew;
         }
-
-        private static void reparar(p_mediana Problema, int[] vs)
-        {
-            double Vt = 0.0;
-            if (!esFactible(vs))
-            {
-                Vt = volumenArmonia(Problema, vs);
-            }
-        }
-
         private static double volumenArmonia(p_mediana Problema, int[] vs)
         {
             double sumar = 0.0;
             for (int i = 0; i < vs.Length; i++)
             {
-                sumar += Problema.ar;
+                sumar++;
             }
             return sumar;
         }
-
-        private static bool esFactible(int[] vs)
+ 
+        public int[] organismoAleatorio(Random myRandom, int n)
         {
-            return false;
+            int[] organismo = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                organismo[i] = valorAleatorio(myRandom);
+            }
+            return organismo;
         }
+        private int valorAleatorio(Random myRandom)
+        {
+            int valor;
+            double alea = myRandom.NextDouble();
+            if (alea < 0.5)
+            {
+                valor = 0;
+            }
+            else
+            {
+                valor = 1;
+            }
+            return valor;
 
+        }
+        public double evaluarSolucion(int[] X)
+        {
+            double[] pesos = new double[] { 2.4, 4.6, 5.3, 5, 1.5, 2.4, 4, 3.2, 1, 0.5 };
+            double evaluacion = 0;
+            for (int i = 0; i < X.Length; i++)
+            {
+                evaluacion = evaluacion + X[i] * pesos[i];
+            }
+            return evaluacion;
+        }
         public override void Ejecutar(p_mediana theProblem, Random myRandom)
         {
-            int[][] HM = 
+            int n = theProblem.numVertices;
+            double HMCR = 1 - (10 / n);
+            int HMS = 5;
+            int[][] HM = new int[HMS][];
+            int vMax; //peso maximo de la mochila
+            int NI= 100; //numero de iteraciones
+            for(int i = 0; i < HMS; i++)
+            {
+                HM[i] = organismoAleatorio(myRandom, n);
+                reparar(theProblem, HM[i]);
+            }
+            int k = 0;
+            while(k < NI)
+            {
+                //Se crea la nueva armonia de tamaño n
+                int[] Xnew = new int[n];
+                for (int i = 0; i < n; i++)
+                {
+                    if(myRandom.NextDouble() <= HMCR)
+                    {
+                        //se escogen dos armonias de manera aleatoria
+                        int[] r1 = HM[myRandom.Next(0, n - 1)];
+                        int[] r2 = HM[myRandom.Next(0, n - 1)];
+                        int[] mejor = mejorHarmonia(HM);
+                        Xnew[i] = r1[i] + (-1) ^ (r1[i]) * (mejor[i]- r2[i]);
+
+                    }
+                    else
+                    {
+                        Xnew[i] = valorAleatorio(myRandom);
+                    }
+
+                }
+                Xnew = reparar(theProblem, Xnew);
+                if(evaluarSolucion(Xnew) < evaluarSolucion(peorHarmonia(HM)))
+                {
+                    int posPeor = posPeorHarmonia(HM);
+                    HM[posPeor] = Xnew;
+                }
+                k++;
+            }
+        }
+        public int posPeorHarmonia(int[][] poblacion)
+        {
+            int[] organismo;
+            int posPeor = 0;
+            double peorEvaluacion = evaluarSolucion(poblacion[0]);
+            for (int i = 1; i < poblacion.Length; i++)
+            {
+                organismo = poblacion[i];
+                double evaluacion = evaluarSolucion(organismo);
+                if (evaluacion > peorEvaluacion)
+                {
+                    posPeor = i;
+                }
+            }
+            return posPeor;
+        }
+        public int[] mejorHarmonia(int[][] memoriaArmonica)
+        {
+            int[] mejor = new int[memoriaArmonica[0].Length];
+            int[] copia = new int[mejor.Length];
+            double mejorEvaluacion = evaluarSolucion(memoriaArmonica[0]);
+            for (int i = 1; i < memoriaArmonica.Length; i++)
+            {
+                copia = memoriaArmonica[i];
+                double evaluacion = evaluarSolucion(copia);
+                if (evaluacion < mejorEvaluacion)
+                {
+                    mejor = copia;
+                }
+            }
+            return mejor;
+        }
+        public int[] peorHarmonia(int[][] memoriaArmonica)
+        {
+            int[] peor = new int[memoriaArmonica[0].Length];
+            int[] copia = new int[peor.Length];
+            double mejorEvaluacion = evaluarSolucion(memoriaArmonica[0]);
+            for (int i = 1; i < memoriaArmonica.Length; i++)
+            {
+                copia = memoriaArmonica[i];
+                double evaluacion = evaluarSolucion(copia);
+                if (evaluacion > mejorEvaluacion)
+                {
+                    peor = copia;
+                }
+            }
+            return peor;
         }
     }
 }
